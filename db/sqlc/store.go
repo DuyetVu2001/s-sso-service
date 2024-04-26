7c729be411e6940767b690b0309ad52b18251071
duyetvn <duyetvn@viettel.com.vn>
2024-04-26 17:10:35 +0700

    store.go file

---
+++ db/sqlc/store.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"github.com/jackc/pgx/v5"
+)
+
+type Store struct {
+	*Queries
+	db *pgx.Conn
+}
+
+func NewStore(db *pgx.Conn) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
