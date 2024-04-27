42ca6fa8b84daa8912db71f869365b9efaa3a3ab
y3dc <vungocduyet24@gmail.com>
2024-04-27 09:06:26 +0700

    add query

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -3,3 +3,4 @@ DROP TABLE IF EXISTS roles cascade;
 DROP TABLE IF EXISTS permissions cascade;
 DROP TABLE IF EXISTS role_permission cascade;
 DROP TABLE IF EXISTS account_permission cascade;
+DROP TABLE IF EXISTS sessions cascade;
